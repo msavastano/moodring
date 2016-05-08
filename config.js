e2a3353bb8e7f2d8282b7345096ba9487cefb6f7
Mike <mikesavastano@gmail.com>
2016-05-07 22:16:44 -0400

    test commit

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54334',
+    'mongoUrl' : 'mongodb://localhost:27017/moodring',
+}
+
+/*
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+
+crypto.randomBytes(256, function(ex, buf) {
+  if (ex) throw ex;
+  var token = jwt.sign({foo: 'bar'}, buf);
+  var decoded = jwt.verify(token, buf);
+});
+
+'facebook': {
+    clientID: '1677631512487156',
+    clientSecret: '9be36079f185955082f47705593237ac',
+    callbackURL: 'https://localhost:3443/users/facebook/callback'
+}
+*/
