c8f7ac541664e3e53a3e04258d3de6da8aba96dc
mike <mikesavastano@gmail.com>
2016-07-30 23:01:27 -0400

    fix prod cloudinary2

--- app.js
+++ app.js
@@ -53,9 +53,9 @@ if (app.get('env') === 'development') {
   });
 }else{
   cloudinary.config({
-    cloud_name: CLOUD_NAME,
-    api_key: CLOUD_API_KEY,
-    api_secret: CLOUD_API_SECRET
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
   });
 }
 
