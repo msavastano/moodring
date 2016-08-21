01c30e3243724145885178d91a9be0c1048808f4
mike <mikesavastano@gmail.com>
2016-08-21 18:19:19 -0400

    pics for moodfeed

--- app_server/controllers/moodfeed_controller.js
+++ app_server/controllers/moodfeed_controller.js
@@ -23,31 +23,42 @@ module.exports.get_mood_feed = function(req, res, next) {
       user.friends.forEach(function(f, fi){
         User.findById(f._id)
           .populate('moods')
+          .populate('moods.comments.postedBy')
+          .populate('moods.comments.commentsOnComments.postedBy')
           .exec(function(err, f){
-            f.moods.forEach(function(m,i){
-              if(m.comments.length > 0){
-                var friendPlusMood = {};
-                friendPlusMood['friend'] = f;
-                friendPlusMood['mood'] = m;
-                friendPlusMoods.push(friendPlusMood);
-              }
-              if(fids.indexOf(f._id) == -1){
-                fids.push(f._id);
-              }
-              if(fids.length == user.friends.length && (i+1) == f.moods.length){
-                console.log("RENDERED");
-                for (i = 0; i < friendPlusMoods.length; i++) {
-                  for (j = 0; j < friendPlusMoods[i]['mood']['comments'].length; j++) {
-                    friendPlusMoods[i]['mood']['comments'][j]['friend'] = friendPlusMoods[i]['friend']['username'];
-                    allComments.push( friendPlusMoods[i]['mood']['comments'][j] );                    
-                  }
+            f.moods.forEach(function(mo,i){
+              Moods.findById(mo._id)
+              .populate('comments.postedBy')
+              .populate('comments.commentsOnComments.postedBy')
+              .exec(function(err, m){
+                if(m.comments.length > 0){
+                  var friendPlusMood = {};
+                  friendPlusMood['friend'] = f;
+                  friendPlusMood['mood'] = m;
+                  friendPlusMoods.push(friendPlusMood);
                 }
-                res.render('moodfeed', {
-                  allComments:allComments,
-                  user : req.decoded._doc.username,
-                  nouser:req.decoded
-                });
-              }
+                if(fids.indexOf(f._id) == -1){
+                  fids.push(f._id);
+                }
+                if(fids.length == user.friends.length && (i+1) == f.moods.length){
+                  console.log("RENDERED");
+                  for (i = 0; i < friendPlusMoods.length; i++) {
+                    for (j = 0; j < friendPlusMoods[i]['mood']['comments'].length; j++) {
+                      friendPlusMoods[i]['mood']['comments'][j]['friend'] = friendPlusMoods[i]['friend']['username'];
+                      friendPlusMoods[i]['mood']['comments'][j]['pic'] = friendPlusMoods[i]['friend']['pic'];
+                      friendPlusMoods[i]['mood']['comments'][j]['friendid'] = friendPlusMoods[i]['friend']['_id'];
+                      allComments.push( friendPlusMoods[i]['mood']['comments'][j] );
+                    }
+                  }
+                  console.log(allComments[2]);
+                  res.render('moodfeed', {
+                    allComments:allComments,
+                    user : req.decoded._doc.username,
+                    nouser:req.decoded,
+                    userpic:req.decoded._doc.pic
+                  });
+                }
+            });
             });
           });
         });
